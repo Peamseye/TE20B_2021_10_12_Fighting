788d03187925233949b35987e1f7938b694f40f3
Peamseye <aispotato587@gmail.com>
2021-10-12 11:03:01 +0200

    Fighting

---
+++ FightingGame/FightingGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+ToolBox.WriteCentered(@" ______                        ______                               ");
+ToolBox.WriteCentered(@"|  ____|                      |  ____|     (_)                      ");
+ToolBox.WriteCentered(@"| |__ __ _ _ __   __   _____  | |__   _ __  _  ___  _   _  ___ _ __ ");
+ToolBox.WriteCentered(@"|  __/ _` | '_ \  \ \ / / __| |  __| | '_ \| |/ _ \| | | |/ _ \ '__|");
+ToolBox.WriteCentered(@"| | | (_| | | | |  \ V /\__ \ | |____| | | | | (_) | |_| |  __/ |   ");
+ToolBox.WriteCentered(@"|_|  \__,_|_| |_|   \_/ |___/ |______|_| |_| |\___/ \__, |\___|_|   ");
+ToolBox.WriteCentered(@"                                          _/ |       __/ |          ");
+ToolBox.WriteCentered(@"                                         |__/       |___/           ");
+
+Console.ReadLine();
+
